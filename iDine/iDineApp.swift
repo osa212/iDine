8f72619bf0a7eb4b4520af65816303740a743fb0
osa212 <78644067+osa212@users.noreply.github.com>
2021-10-06 20:02:18 +0900

    Initial Commit

---
+++ iDine/iDineApp.swift
@@ -0,0 +1,17 @@
+//
+//  iDineApp.swift
+//  iDine
+//
+//  Created by Sargylana Osipova on 06.10.2021.
+//
+
+import SwiftUI
+
+@main
+struct iDineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
